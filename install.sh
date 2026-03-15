8c0952771868dce0adf726c68a5e5c0946620f21
danilo <danilo@Kali.Kali>
2026-03-14 21:18:45 -0300

    Terminando el script de configuracion de kali

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ blue="\u001b[0;34m"
 readonly ruta=$(realpath $0 | rev | cut -d'/' -f2- | rev)
 readonly user="${USER}"
 readonly log="${HOME}/autobspwm.log"
+distro="Kali"
 source ./Colors
 source ./utils/ask_yes_no.sh
 source ./utils/messagebox.sh
@@ -49,7 +50,7 @@ welcome() {
                     0M.                 .:o.
                     ;Wd
                      ;XO,
-                       ,d0Odlc;,..
+,d0Odlc;,..
                            ..',;:cdOOd::,.
                                     .:d;.':;.
                                        'd,  .'
@@ -138,7 +139,7 @@ bspwm() {
 		done
 
 		cd "${ruta}" || return
-		sudo rm -rf /tmp/ImageMagick* &>"${LOGS}"
+		sudo rm -rf /tmp/ImageMagick* &>"${log}"
 
 		[[ ! -d "${HOME}/Imágenes/" ]] && mkdir -p ~/Imágenes
 		[[ ! -d "${HOME}/Imágenes/capturas" ]] && mkdir -p ~/Imágenes/capturas
@@ -168,8 +169,7 @@ bspwm() {
 
 }
 
-install_sxhkd() {
-	SECONDS=0
+sxhkd() {
 	(
 		cd "${ruta}"
 		rm -rf ~/.config/sxhkd/ 2>"${log}"
@@ -272,7 +272,7 @@ install_sxhkd() {
 
 	wait "${PID}"
 
-	set_time "${SECONDS}" "Sxhkd instalado de forma correcta"
+	set_time 1 " Sxhkd instalado de forma correcta"
 
 }
 
@@ -288,8 +288,299 @@ install_git() {
 	fi
 }
 
+polybar() {
+	cd "${ruta}" || exit 1
+
+	(
+		sudo apt install polybar -y &>/dev/null
+		rm -rf ~/.config/polybar/ 2>"${log}"
+		cp -r ./config/polybar/ ~/.config/
+
+		sudo apt install libnotify-bin -y &>/dev/null
+		sudo apt install dunst -y &>/dev/null
+
+		cp -r ./config/dunst ~/.config/ &>/dev/null
+	) &
+
+	PID=$!
+
+	spinner_log "${bright_white}Instalando${bright_magenta} Polybar${bright_white}, esto podria tomar un tiempo${end}" "0.2" "${PID}"
+
+	wait "${PID}"
+
+	set_time 1 "La Polybar se instalo de forma correcta"
+
+}
+
+picom() {
+	cd "${ruta}" || exit 1
+
+	(
+		sudo apt install meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libev-dev libpcre3-dev -y &>/dev/null
+		sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev -y &>/dev/null
+		sudo apt install cmake -y &>/dev/null
+
+		[[ -d "picom" ]] && rm -rf picom
+		if ! sudo apt install picom -y &>/dev/null; then
+			# Instalamos picom desde los repositorios de git
+			git clone https://github.com/yshui/picom &>/dev/null
+			cd picom || return
+			meson setup --buildtype=release build &>/dev/null
+			ninja -C build &>/dev/null
+			sudo cp build/src/picom /usr/local/bin &>/dev/null
+			sudo cp build/src/picom /usr/bin/ &>/dev/null
+			cd ..
+			rm -rf picom &>/dev/null
+		fi
+		cd "${ruta}"
+
+		cp -r ./config/picom/ ~/.config/ &>/dev/null
+
+	) &
+
+	PID=$!
+
+	spinner_log "${bright_white}Instalando${bright_magenta} Picom${bright_white}, esto podria tomar un tiempo${end}" "0.2" "${PID}"
+
+	wait "${PID}"
+
+	set_time 1 "Picom se instalo de forma correcta"
+
+}
+
+fonts() {
+	cd "${ruta}" || exit 1
+	[[ ! -d "${HOME}/.local/share/fonts/" ]] && mkdir -p ~/.local/share/fonts &>/dev/null
+	(
+		sudo cp -r fonts/* /usr/local/share/fonts &>/dev/null
+		sudo cp -r fonts/* ~/.local/share/fonts &>/dev/null
+		sudo cp -r fonts/* /usr/share/fonts/truetype/ &>/dev/null
+		sudo cp ./config/polybar/fonts/* /usr/share/fonts/truetype &>/dev/null
+		sudo apt install -y papirus-icon-theme &>/dev/null
+		sudo apt install fonts-noto-color-emoji -y &>/dev/null
+		fc-cache -vf &>/dev/null
+	) &
+
+	PID=$!
+
+	spinner_log "${bright_white}Instalando las fuentes necesarias${end}" "0.2" "${PID}"
+
+	wait "${PID}"
+
+	set_time 1 "${bright_white}Las fuentes se instalaron de forma correcta"
+
+}
+eww() {
+	cd "${ruta}" || return 1
+	(
+		if [[ "${distro}" == "Parrot" ]]; then
+			for i in {1..3}; do
+				sudo apt install "libgtk-3-dev" -y 2>&1 | grep -Po '(\S+) \(= [^)]+\)' | sed -E 's/([^ ]+) \(= ([^)]+)\)/\1=\2/' | xargs -r sudo apt install --allow-downgrades -y &>/dev/null
+			done
+		fi
+		# Instalamos eww y sus dependencias
+		sudo apt install -y \
+			git build-essential pkg-config \
+			libgtk-3-dev libpango1.0-dev libglib2.0-dev libcairo2-dev \
+			libdbusmenu-glib-dev libdbusmenu-gtk3-dev \
+			libgtk-layer-shell-dev \
+			libx11-dev libxft-dev libxrandr-dev libxtst-dev &>/dev/null
+
+		# Si hay un directorio eww lo borramos entero
+		[[ -d "eww" ]] && rm -rf "eww"
+
+		git clone https://github.com/elkowar/eww.git &>/dev/null
+		cd eww
+
+		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &>/dev/null
+		source ${HOME}/.cargo/env &>/dev/null
+		cargo clean &>/dev/null
+		cargo build --release &>/dev/null
+
+		if [[ $? -eq 0 ]]; then
+			sudo cp target/release/eww /usr/bin/
+			mkdir -p ~/.config/eww
+			cd ..
+			[[ -d "eww" ]] && rm -rf eww
+			# Traemos la configuración de eww
+			cp -r ./config/eww/ ~/.config/
+		fi
+
+		DIR="/tmp/RIPGREP"
+		# Instalamos ripgrep
+		if ! sudo apt install ripgrep -y &>/dev/null; then
+			rm -rf "${DIR}" &>/dev/null
+			mkdir -p "${DIR}" &>/dev/null
+			curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &>/dev/null
+			source "$HOME/.cargo/env" &>/dev/null
+			cd "${DIR}" &>/dev/null
+			git clone https://github.com/BurntSushi/ripgrep.git &>/dev/null
+			cd ripgrep &>/dev/null
+			cargo build --release &>/dev/null
+			sudo cp target/release/rg /usr/local/bin/ &>/dev/null
+		fi
+
+		rm -rf "${DIR}" &>/dev/null
+	) &
+
+	PID=$!
+
+	spinner_log "${bright_white}Instalando eww${end}" "0.2" "${PID}"
+
+	wait "${PID}"
+
+	set_time 1 "${bright_white}Eww se instalo de forma correcta"
+
+}
+
+magick() {
+	cd "${ruta}" || return
+	(
+		sudo apt install -y build-essential checkinstall \
+			libx11-dev libxext-dev zlib1g-dev libpng-dev \
+			libjpeg-dev libfreetype6-dev libxml2-dev \
+			libtiff-dev libwebp-dev libopenexr-dev \
+			libheif-dev libraw-dev liblcms2-dev \
+			ghostscript curl &>/dev/null
+
+		cd /tmp || exit 1
+		rm -rf ImageMagick-* &>/dev/null
+
+		sudo apt install -y build-essential checkinstall \
+			libx11-dev libxext-dev zlib1g-dev libpng-dev libjpeg-dev \
+			libfreetype6-dev libxml2-dev libtiff-dev libwebp-dev \
+			libfontconfig1-dev libopenexr-dev libltdl-dev git -y &>/dev/null
+
+		wget https://imagemagick.org/archive/ImageMagick.tar.gz &>/dev/null
+		tar xvzf ImageMagick.tar.gz &>/dev/null
+		cd ImageMagick-* || return 1
+
+		./configure --with-modules --enable-shared \
+			--with-fontconfig=yes \
+			--with-freetype=yes \
+			--with-jpeg=yes \
+			--with-png=yes \
+			--with-tiff=yes \
+			--with-webp=yes &>/dev/null
+
+		make -j"$(nproc)" &>/dev/null
+
+		sudo make install &>/dev/null
+		sudo ldconfig &>/dev/null
+
+		cd "${ruta}"
+		# [[ "${distro}" == "Kali" ]] && ./upgrader --magick &>/dev/null
+	) &
+
+	PID=$!
+
+	spinner_log "${bright_white}Instalando imagemagick${end}" "0.2" "${PID}"
+
+	wait "${PID}"
+
+	show_timestamp 1 "${bright_white}Imagemagick se instalo de forma correcta"
+
+}
+
+rofi() {
+	(
+		sudo apt install -y rofi &>/dev/null
+		sudo apt install -y thunar &>/dev/null
+		cp -r ./config/rofi/ ~/.config/
+		sudo apt install librsvg2-common -y &>"${LOGS}"
+		sudo apt install libgtk-3-bin -y &>"${LOGS}"
+
+		sudo update-icon-caches /usr/share/icons/* &>/dev/null
+		sudo gtk-update-icon-cache /usr/share/icons/Papirus-Dark &>/dev/null
+	) &
+
+	PID=$!
+
+	spinner_log "${bright_white}Instalando rofi${end}" "0.2" "${PID}"
+
+	wait "${PID}"
+	>?
+	show_timestamp "${SECONDS}" "${bright_white}Rofi se instalo de forma correcta"
+
+	cd "${ruta}" || return
+
+	THEMES=(Default Latte Macchiato Frappe Mocha)
+
+	if [[ -z "${NICKNAME}" ]]; then
+		echo -ne "\r${bright_cyan:-}[+]${bright_white:-} Introduce el nick que se vera reflejado en el fondo de pantalla: "
+		read -r NICK_INPUT
+		NICKNAME="${NICK_INPUT:-${USER}}"
+	fi
+
+	for THEME in "${THEMES[@]}"; do
+		./font.sh \
+			--input-image "./wallpapers/Themes/${THEME}/HTB.jpg" \
+			--output-image "./wallpapers/Themes/${THEME}/Wallpaper.jpg" \
+			--font-path "/usr/share/fonts/truetype/HackNerdFont-Regular.ttf" \
+			--fill white \
+			--nickname "${NICKNAME}" &>/dev/null
+
+	done
+
+	echo -e "\n${bright_green:-}[✓]${bright_white:-} Imagenes generadas, nombre de usuario:${bright_magenta:-} ${NICKNAME}${end:-}\n"
+
+	mkdir -p ~/Imágenes/wallpapers/
+	cp -r ./wallpapers/* ~/Imágenes/wallpapers/
+
+	echo -ne "${bright_yellow}[+]${bright_white} La instalación del entorno ha finalizado, deseas reiniciar?${end}${bright_magenta} (Y/n)${end} " && read -r confirm
+
+	if [[ "${confirm}" =~ ^[YySs] ]]; then
+		sudo systemctl reboot
+	fi
+
+}
+
+gnome_install() {
+	BACKUP="$HOME/Kali-dotfile/gnome-backup/"
+	EXTENSION="$HOME/Kali-dotfile/gnome-backup/extensions/"
+	apt install -y gnome-shell-extensions
+	dconf load / <"$BACKUP"*.ini
+	cp -r "$EXTENSION"* ~/.local/share/gnome-shell/extensions 2>/dev/null
+	if command -v systemctl &>/dev/null; then
+		systemctl --user restart gnome-shell 2>/dev/null || true
+	fi
+	killall -3 gnome-shell 2>/dev/null || true
+	sleep 2
+	spinner_log "${bright_blue}Instalando gnome y configuraciones "
+	set_time 1 "${bright_blue}Se configuro Gnome de forma correcta ${end}"
+}
+
+config() {
+	local config=("bat" "bspwm" "ctk" "dunst" "eww" "fastfetch"
+		"gtk-3.0" "jgmenu" "kitty" "lazydocker" "lazygit" "nvim" "polybar"
+		"powerlevel10k" "rofi" "sxhkd" "zsh" "zsh-sudo" "zsh-autosuggestions" "superfile")
+	for conf in "${config[@]}"; do
+		if [ -d "${HOME}/.config/$conf" ]; then
+			ln -sf "${HOME}/.config/$conf" "/root/.config/$conf"
+		fi
+	done
+	spinner_log "${bright_blue}Instalando configuraciones "
+	set_time 1 "${bright_blue}se instalaron todas las configuraciones ${end}"
+}
+
+htb() {
+	pipx install htb-operator
+	spinner_log "${bright_blue}Innstalando htb-operator"
+	set_time 1 "${bright_blue}Se instalo con exito ${end}"
+}
+
 welcome
 system_update
 brew
 bspwm
+sxhkd
+polybar
+picom
+eww
+magick
+rofi
 install_git
+gnome_install
+fonts
+config
+htb
