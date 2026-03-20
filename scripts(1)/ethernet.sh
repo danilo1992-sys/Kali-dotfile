8834993b595ccfabd633c0facc2cc7a0da25e436
danilo1992-sys <sanfracaceresdanilo@gmail.com>
2026-03-20 13:42:19 -0300

    refactor: normalize indentation in install.sh and reorganize scripts

--- scripts(1)/ethernet.sh
+++
@@ -1,9 +0,0 @@
-echo "  "
-#!/bin/bash
-
-if ip link show eth0 &>/dev/null; then
-  IP=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-  echo "󰈀   ${IP}"
-else
-  echo "󰈀   Disconect"
-fi
